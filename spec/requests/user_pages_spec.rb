7a63fa1f6064b27a3953e097ed67f730b77129c1
Szymon Zielinski <zelsin@yahoo.co.uk>
2014-05-03 08:19:36 +0100

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -25,8 +25,8 @@ describe "UserPages" do
 
 		describe "with invalid information" do
 			it "should not create a user" do
-				expect {click_button submit}.not_to change(User, :count)
-			end
+        		expect { click_button submit }.not_to change(User, :count)			
+        	end
 		end
 
 		describe "with valid information" do
