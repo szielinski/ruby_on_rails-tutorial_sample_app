8f1d64e7256ac9148d4cfdfdce81820fc1c29751
Szymon Zielinski <zelsin@yahoo.co.uk>
2014-05-12 19:55:11 +0100

    Finished ch10

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -27,8 +27,43 @@ describe "MicropostPages" do
       it "should create a micropost" do
         expect { click_button "Post" }.to change(Micropost, :count).by(1)
       end
+
+      describe "micropost count" do
+        before { user.microposts.delete_all }
+        it { should have_content("0 microposts")}
+
+        describe "one micropost" do
+          before do
+            FactoryGirl.create(:micropost, user: user)
+            visit root_path
+          end
+          it { should have_content("1 micropost")}
+        end
+
+        describe "multiple microposts" do
+          before do
+            2.times { FactoryGirl.create(:micropost, user: user) }
+            visit root_path
+          end
+          it { should have_content("2 microposts")}
+        end
+      end
     end
   end
+
+  describe "pagination" do
+    before { 
+      user.microposts.delete_all
+      31.times { FactoryGirl.create(:micropost, user:user) } 
+      visit root_path
+    }
+    after(:all)  { user.microposts.delete_all }
+    
+    it { should have_selector('div.pagination') }
+  
+    #needs extra work to check for contents
+  end
+
   describe "micropost destruction" do
     before { FactoryGirl.create(:micropost, user: user) }
 
@@ -38,6 +73,16 @@ describe "MicropostPages" do
       it "should delete a micropost" do
         expect { click_link "delete" }.to change(Micropost, :count).by(-1)
       end
+    end
+
+    describe "as incorrect user" do
+      let(:other_user) { FactoryGirl.create(:user) }
+
+      before {
+        FactoryGirl.create(:micropost, user: other_user)
+        visit user_path(other_user)
+      }
+      it { should_not have_link "delete"}
     end
   end  
 end
